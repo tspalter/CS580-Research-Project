cbd0b6be9c048910b3f4661fe8a16b63eb40758c
jarred.eagley <jarred.eagley@gmail.com>
2022-03-14 01:19:59 -0400

    Committed a starter unreal project.

---
+++ Source/researchProject/researchProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "researchProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RESEARCHPROJECT_API AresearchProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
